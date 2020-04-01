c5ced9ab3c0bedd67947c97248da820c0515a064
Matiko <877201213@qq.com>
2020-04-01 09:42:54 +0800

    增加home组件及路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './../components/Login.vue'
+import Home from './../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
   ]
 })
 
