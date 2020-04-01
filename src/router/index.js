19e20e2d2b7b5bb426620906f6c347ca5a3acd9e
Matiko <877201213@qq.com>
2020-04-01 10:14:51 +0800

    增加了路由导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -12,5 +12,18 @@ const router = new VueRouter({
     { path: '/home', component: Home }
   ]
 })
+// 挂载路由导航守卫。
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // form 代表，从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //       next() 放行    next('/login') 强制跳转
+  if (to.path === '/login') return next()
+  // 先获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token， 强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
 
 export default router
